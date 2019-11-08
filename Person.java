70c97bb9941561f4c99463077025a54a65d3c238
iainlambie <ila@gcu.ac.uk>
2019-11-08 16:02:00 +0000

    First commit of Person class

---
+++ Person.java
@@ -0,0 +1,32 @@
+/*
+	Class Person is a superclass that
+	will be used when building a class hierarchy
+	of specific classes to model specifc "People"
+	related beviour
+*/
+
+public class Person
+{
+	private String firstName;
+	private String otherNames;
+	private String surname;
+
+	public Person()
+	{
+		firstName = "";
+		othernames = "";
+		surname = "";
+
+	} // End of constructor
+
+	public Person(String value1, String value2, String value3)
+	{
+		firstNames = value1;
+		otherNames = value2;
+		surname = value3;
+
+	} // End of Constructor
+
+	// Getters and setters will go here in pairs
+
+} // End of class declaration
